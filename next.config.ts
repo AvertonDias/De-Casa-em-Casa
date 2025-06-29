e21249c95453ae85b74c0da297d9d6a7e06cffa6
Averton Silva Dias <verton3@gmail.com>
2025-06-29 17:43:32 +0000

    Abra o Arquivo de Configuração No seu editor do IDX, abra o arquivo next

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import withPWA from '@ducanh2912/next-pwa';
 const pwaPlugin = withPWA({
   dest: 'public',
   register: true,
-  skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
 });
 
