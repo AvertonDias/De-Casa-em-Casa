849758cdf12eadb1ff7b461201c1c35e11843989
Averton Silva Dias <verton3@gmail.com>
2025-06-30 14:37:57 +0000

    Minha nova atualização

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Qualquer configuração específica do Next.js entra aqui.
-  // Por enquanto, pode ficar vazio.
-};
+import withPWA from '@ducanh2912/next-pwa';
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+const pwaConfig = withPWA({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+});
+
+// Esta é a forma mais segura de combinar as configurações
+const finalConfig = pwaConfig(nextConfig);
+
+export default finalConfig;
