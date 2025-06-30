65eabe7c0dc333d824155d789588a065ffd7884c
Averton Silva Dias <verton3@gmail.com>
2025-06-30 14:51:08 +0000

    feat: Install and configure next-pwa

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const withPWA = require('@ducanh2912/next-pwa')({
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+  });
+  
+  /** @type {import('next').NextConfig} */
+  const nextConfig = {
+    // Qualquer configuração futura do Next.js entra aqui.
+    // Por enquanto, pode ficar vazio.
+  };
+  
+  module.exports = withPWA(nextConfig);
