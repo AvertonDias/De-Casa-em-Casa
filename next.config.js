49307ae79635e2e411fd107cd8b7610f3e98445e
AvertonDias <45372751+AvertonDias@users.noreply.github.com>
2025-06-30 13:03:13 -0300

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-const withPWA = require('@ducanh2912/next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-});
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = withPWA(nextConfig);
