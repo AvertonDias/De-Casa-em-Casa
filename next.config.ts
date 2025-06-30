896ce0325a0c0b51ab6cce0146b2d490ebfb1d53
AvertonDias <45372751+AvertonDias@users.noreply.github.com>
2025-06-30 11:37:04 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,14 @@
-import withPWAInit from '@ducanh2912/next-pwa';
+import withPWA from '@ducanh2912/next-pwa';
 
-// Primeiro, chame a função importada com as suas opções de PWA.
-// Isso retorna a função que irá "embrulhar" sua configuração do Next.js.
-const withPWA = withPWAInit({
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+const pwaConfig = withPWA({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
 });
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Qualquer configuração específica do Next.js entra aqui.
-  // Por enquanto, pode ficar vazio.
-};
+// Esta é a forma mais segura de combinar as configurações
+const finalConfig = pwaConfig(nextConfig);
 
-// Por fim, exporte o resultado da função PWA chamada com a sua configuração do Next.js.
-export default withPWA(nextConfig);
+export default finalConfig;
