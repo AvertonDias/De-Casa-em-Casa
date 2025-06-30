a4379c4abee1aaf35c2ca7cc5a90d78a53273d8e
Averton Silva Dias <verton3@gmail.com>
2025-06-30 14:29:32 +0000

    FIX: Convert next.config.ts to use ES Modules

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
-const withPWA = require('@ducanh2912/next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-});
+import withPWA from '@ducanh2912/next-pwa';
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Configurações do Next.js podem ser adicionadas aqui no futuro
+};
 
-module.exports = withPWA(nextConfig);
+// O export default usa a função withPWA para "embrulhar" sua configuração do Next.js
+export default withPWA({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+})(nextConfig);
