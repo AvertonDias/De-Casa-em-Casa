c7f92102a6d8ca66805d1ff653f6e4e59285159a
Averton Silva Dias <verton3@gmail.com>
2025-07-01 20:18:40 +0000

    Lista de Verificação Final ("Pre-Flight Checklist") 1. Arquivo next.conf

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('@ducanh2912/next-pwa')({
+const withPWA = require('@ducanh2912/next-pwa');
+
+const nextConfig = {
+  // Next.js configuration options can be added here.
+};
+
+module.exports = withPWA({
   dest: 'public',
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
-});
-
-const nextConfig = {
-  // Objeto vazio, sem a chave 'experimental'.
-};
-
-module.exports = withPWA(nextConfig);
+})(nextConfig);
