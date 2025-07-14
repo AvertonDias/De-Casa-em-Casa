483219eeed539509db11338914de0e5671642351
Averton Silva Dias <verton3@gmail.com>
2025-07-14 16:55:50 +0000

    ficou muito grande, diminua um pouco

--- src/components/ImagePreviewModal.tsx
+++ src/components/ImagePreviewModal.tsx
@@ -32,6 +32,7 @@ export default function ImagePreviewModal({ isOpen, imageUrl, onClose }: ImagePr
           src={imageUrl} 
           alt="Preview do Cartão em tela cheia" 
           // Rotação aplicada em telas pequenas (smartphones)
+          // AQUI A MUDANÇA: Reduzi o tamanho máximo para 80vw, um pouco menor.
           className="max-w-screen-xl max-h-[90vh] object-contain md:max-h-[90vh] md:max-w-[90vw] md:rotate-0 rotate-90 max-h-[80vw] max-w-[80vw] transition-transform duration-300"
         />
       </div>
