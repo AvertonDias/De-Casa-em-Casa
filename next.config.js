b539618fe60f847fa431ca99273ad630918f15ad
Averton Silva Dias <verton3@gmail.com>
2025-07-30 00:35:40 +0000

    Na raiz do seu projeto, abra o arquivo next.config.js. Apague TODO o con

--- next.config.js
+++ next.config.js
@@ -1,28 +1,24 @@
-// next.config.js
+// Usamos a sintaxe de import, como no resto do seu projeto
+import withPWAInit from "@ducanh2912/next-pwa";
 
-const withPWA = require("@ducanh2912/next-pwa")({
+const withPWA = withPWAInit({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
-  register: true,
-  skipWaiting: true,
   importScripts: ["/firebase-messaging-sw.js"],
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  // A configuração de imagens que permite o uso de placehold.co
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'placehold.co',
       },
-      {
-        protocol: 'https',
-        hostname: 'storage.googleapis.com',
-      }
     ],
   },
 };
 
-module.exports = withPWA(nextConfig);
+// E exportamos usando a sintaxe 'export default'
+export default withPWA(nextConfig);
