ccc909a256ecbac41fd44f83d58fbfaf88cf9ff5
Averton Silva Dias <verton3@gmail.com>
2025-08-09 20:04:47 +0000

    Abra o arquivo next.config.js no seu editor (Open Editor). Ele provavelm

--- next.config.js
+++ next.config.js
@@ -10,6 +10,13 @@ const isDev = process.env.NODE_ENV === "development";
 const withPWA = withPWAInit({
   dest: "public",
   disable: isDev,
+  // The service worker filename is the default, but we specify it for clarity.
+  sw: "sw.js", 
+  // The service worker is registered by default, but we can be explicit.
+  register: true, 
+  // We can skip waiting for the old service worker to unregister.
+  skipWaiting: true, 
+  // Custom import to load the Firebase messaging service worker
   importScripts: ["/firebase-messaging-sw.js"],
 });
 
