bff6ffa8c53226aaaaff7fa2822e4574d7317fb8
Averton Silva Dias <verton3@gmail.com>
2025-08-16 14:16:58 +0000

    Abra o arquivo next.config.js no editor (Open Editor). Procure por ERROS

---
+++ home/avd/de-casa-em-casa/next.config.js
@@ -0,0 +1,30 @@
+
+// Configure dotenv to load environment variables at the very top
+require('dotenv').config({ path: './.env.local' });
+
+const withPWAInit = require("@ducanh2912/next-pwa");
+
+const isDev = process.env.NODE_ENV === "development";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: isDev,
+  sw: "sw.js",
+  register: true,
+  skipWaiting: true,
+  importScripts: ["/firebase-messaging-sw.js"],
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'placehold.co' },
+    ],
+  },
+  experimental: {
+    forceSwcTransforms: true,
+  },
+};
+
+module.exports = withPWA(nextConfig);
