2751974dc3ef697ba08e90e4524dd89debc9ce6d
Averton Silva Dias <verton3@gmail.com>
2025-10-31 22:22:52 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ import pwa from '@ducanh2912/next-pwa';
 const withPWA = pwa({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
+  // add your own strategies
+  // cacheOnFrontEndNav: true,
+  // aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  workboxOptions: {
+    disableDevLogs: true,
+  }
 });
 
 const nextConfig = {
