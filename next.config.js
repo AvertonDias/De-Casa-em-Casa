280c76616e2afb329d0f164513ec849aa3363bbf
Averton Silva Dias <verton3@gmail.com>
2025-11-11 23:49:54 +0000

    a função pwa está funcionando?

--- next.config.js
+++ next.config.js
@@ -3,10 +3,6 @@ import pwa from '@ducanh2912/next-pwa';
 
 const withPWA = pwa({
   dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  // add your own strategies
-  // cacheOnFrontEndNav: true,
-  // aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
   swcMinify: true,
   workboxOptions: {
