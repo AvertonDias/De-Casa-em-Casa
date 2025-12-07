41d04f3ce4179f94375dd594abbbf1532366b403
Averton Silva Dias <verton3@gmail.com>
2025-12-07 17:00:44 +0000

    pode gerar o arquivo pq o resto da pasta android vou fazer fora daqui no

---
+++ capacitor.config.ts
@@ -0,0 +1,14 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.decasaemcasa.app',
+  appName: 'De Casa em Casa',
+  webDir: 'out',
+  plugins: {
+    PushNotifications: {
+      presentationOptions: ["badge", "sound", "alert"],
+    },
+  },
+};
+
+export default config;
