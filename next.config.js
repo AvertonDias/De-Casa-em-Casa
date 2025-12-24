6baaa174a90ad183c8ae312ee40bae06da9ff64c
Averton Silva Dias <verton3@gmail.com>
2025-12-24 01:24:10 +0000

    studio-6319583051:~/studio{main}$ npm rum build
    > nextn@0.1.0 build
    > n

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {},
+  // Adiciona a configuração para ignorar a página do favicon durante o build
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+  webpack: (config, { isServer }) => {
+    // Adiciona uma regra para ignorar a rota do favicon do lado do servidor
+    if (isServer) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        './favicon.ico': false,
+      };
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
