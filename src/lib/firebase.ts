d9ce414fbca6aaa2291c9c8f197e05ac451427ed
Averton Silva Dias <verton3@gmail.com>
2025-12-27 18:24:27 +0000

    main-app.js?v=1766859773456:1847 Download the React DevTools for a bette

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,4 +1,3 @@
-
 // src/lib/firebase.ts
 import { initializeApp, getApps, getApp, type FirebaseApp } from "firebase/app";
 import { getAuth, type Auth, browserLocalPersistence, setPersistence } from "firebase/auth";
