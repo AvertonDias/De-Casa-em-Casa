0dcaffd4d7fbe0a8bb0d921678e3e86265465053
Averton Silva Dias <verton3@gmail.com>
2025-12-30 19:18:10 +0000

    Para o app nativo Android oq devo fazer? Tenho q mar oq na pasta Android

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const nextConfig = {
   images: {},
   experimental: {
     missingSuspenseWithCSRBailout: false,
-    instrumentationHook: false, // Adicionado para forçar a invalidação do cache
   },
 };
 
