1d20b5c65a28e07415e15a5c52bc9a22af147543
Averton Silva Dias <verton3@gmail.com>
2026-04-27 23:02:15 +0000

    https://youtube.com/shorts/9SM5oP5qmcc?feature=share (_for element src/a

--- src/lib/tutorials.ts
+++ src/lib/tutorials.ts
@@ -6,7 +6,7 @@ export const TUTORIAL_IDS = {
   ACCEPT_USER: 'X9tWNXo7JBK', // Tutorial: Aceitar usuário pendente
   REGISTER: 'HpyQfjTRp59',    // Tutorial: Cadastro no app
   SHARE_APP: 'dotprompt24',   // Tutorial: Compartilhar app
-  MANAGE_HOUSES: 'genkit170', // Tutorial: Gerenciar, editar e reordenar casas
+  MANAGE_HOUSES: '9SM5oP5qmcc', // Tutorial: Gerenciar, editar e reordenar casas (URL: https://youtube.com/shorts/9SM5oP5qmcc)
 };
 
 /**
