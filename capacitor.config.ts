6a8efdf66c1973e47ef2a594ccd8b9c6f946133d
AvertonDias <45372751+AvertonDias@users.noreply.github.com>
2026-07-25 15:04:37 -0300

    Android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,6 @@ const config: CapacitorConfig = {
   appId: 'com.decasaemcasa.app',
   appName: 'De Casa em Casa',
   webDir: 'out',
-  bundledWebRuntime: false,
   server: {
     androidScheme: 'https',
     allowNavigation: ['*']
--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {},
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
